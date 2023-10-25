35010f9b6023f97e15f5d0f836e24fbbf4296e41
Karolalbr <karol.albrechtowicz@microsoft.wsei.edu.pl>
2023-10-25 14:51:35 +0200

    update-1

---
+++ labolatorium/Controllers/BirthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace labolatorium.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(Birth model)
+        {
+            if (ModelState.IsValid)
+            {
+                DateTime currentDate = DateTime.Now;
+                int age = currentDate.Year - model.BirthDate.Year;
+                ViewBag.Message = $"Cześć {model.Name}, masz {age} lat(a).";
+                return View("Result");
+            }
+            return View("Form", model);
+        }
+    }
+}
---
+++ labolatorium/Models/Birth.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Birth
+{
+    [Required(ErrorMessage = "Imię jest wymagane.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Data urodzenia jest wymagana.")]
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+}
